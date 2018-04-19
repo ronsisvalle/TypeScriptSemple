54f09b0058f9c91ce4cde32bc66f0e1442cd6c44
Franesco_Ronsisvalle <f.ronsisvalle@aubay.it>
2018-04-19 19:27:30 +0200

    Inseriti Promise e Let

--- aubay-es6-simple/es6.ts
+++ aubay-es6-simple/es6.ts
@@ -70,6 +70,25 @@ for (var i = 1; i < 5; i++) {
     console.log(i)
     logLater(i)
 };
+//TODO LET
+var x = 1;
+
+if(x < 10) {
+	let v = 1;
+	v = v + 21;
+	v = v * 100;
+	v = v / 8;
+
+	console.log(v);
+}
+
+console.log(v);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i); // 0, 1, 2, 3, 4 ... 9
+}
+
+console.log(i); // i is not defined
 */
 // //TODO ===
 // var num = 0;
@@ -100,9 +119,47 @@ var somma = (x, y) => {
     return x + y
 };
 () => console.log("Hello world!");
+//TODO Functions Rest Parameters
+//The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
+function sum(...theArgs) {
+  return theArgs.reduce((previous, current) => {
+    return previous + current;
+  });
+}
+
+console.log(sum(1, 2, 3));
+// expected output: 6
+
+console.log(sum(1, 2, 3, 4));
+// expected output: 10
+
 //TODO creating an array from the messages array:
 var ids = ['5632953c4e345e145fdf2df8', '563295464e345e145fdf2df9'];
 var messages = ids.map(function (value) {
     return "ID is " + value // explicit return
 });
 var messages = ids.map(value => `ID is ${value}`); // implicit return*/
+//TODO Promise
+/*
+var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async Work Complete");
+        resolve();
+    }, 1000);
+});
+promise.then(
+    (dataResp) => {
+        console.log("Success", dataResp);
+    },
+    (error) => {
+        console.log("Error", error);
+    }
+);
+promise.then(
+    (dataResp) => {
+        console.log("Success", dataResp);
+    },
+    (error) => {
+        console.log("Error", error);
+    }
+);*/
